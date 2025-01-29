bf5b6d2aa38a1d1cb7c0b2971d47d4b16679fd96
Augusto <augustocesarrezende64@gmail.com>
2025-01-29 17:11:46 -0300

    Item page updated

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,21 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 interface ItemProps {
-    name: string;
-    category: string;
-    price: number;
-    details: string;
-    img: string;
-  }
-
-const Item = ({ name, category , price , details, img } : ItemProps) => {
-  return (
-    <div className="flex flex-col items-center justify-center p-5">
-      <img src={img} alt={name} className="w-4/5 h-60 rounded-2xl object-contain" />
-      <h2 className="text-lg font-bold">{name}</h2>
-      <p className="text-sm text-gray-500">{category}</p>
-      <p className="text-green-600 font-semibold">${price.toFixed(2)}</p>
-      <p className="text-gray-700 text-xs mb-5">{details}</p>
-    </div>
-  )
+  name: string;
+  category: string;
+  price: number;
+  details: string;
+  img: string;
 }
 
-export default Item
+const Item = ({ name, category, price, details, img }: ItemProps) => {
+  return (
+    <div className="flex flex-col items-start  w-4/5 m-auto rounded-2xl bg-white justify-center p-5">
+      <div className="flex justify-between items-center w-full">
+        <h2 className="text-4xl mr-10 font-bold">{name}</h2>
+        <div className="rounded-3xl h-full">
+          <img
+            src={img}
+            alt={name}
+            className="rounded-2xl h-44 object-contain"
+          />
+        </div>
+      </div>
+      <input
+        type="button"
+        className="text-lg text-green-600 font-bold my-5 ml-5"
+        value="Shop now -> "
+      />
+    </div>
+  );
+};
+
+export default Item;
