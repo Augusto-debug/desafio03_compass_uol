0c504d56851e5ecc51de6a01e7ab16d69d9bfc55
Augusto <augustocesarrezende64@gmail.com>
2025-01-29 07:45:03 -0300

    signIn and Signup layout fixed

--- src/components/SignInScreen.tsx
+++ src/components/SignInScreen.tsx
@@ -1,38 +1,48 @@
-  import AuthButton from "./AuthButton";
-  import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import AuthButton from "./AuthButton";
 
-  const SignScreen = () => {
-    const navigate = useNavigate();
+const SignScreen = () => {
+  const navigate = useNavigate();
 
-    const handleSignUp = () => {
-      navigate("/signup");
-    };
-
-    return (
-      <div className="w-full h-screen flex items-center justify-center flex-col relative">
-        <img
-          src="/backgroundImageLoginScreen.svg"
-          alt="background image"
-          className="w-full h-full object-cover absolute"
-        />
-        <div className="absolute top-28 w-full flex items-center justify-center flex-col">
-          <p className="text-9xl">Audio</p>
-          <p className="text-3xl py-10">It's modular and designed to last</p>
-        </div>
-        <div className="absolute top-1/2 w-full flex flex-col items-center">
-          <AuthButton mode="login" />
-          <p className="mt-5">
-            Don't have an account?{" "}
-            <span
-              onClick={handleSignUp}
-              className="text-green-600 cursor-pointer"
-            >
-              Sign Up Here
-            </span>
-          </p>
-        </div>
-      </div>
-    );
+  const handleSignUp = () => {
+    navigate("/signup");
   };
 
-  export default SignScreen;
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col relative">
+      <img
+        src="/backgroundImageLoginScreen.svg"
+        alt="background image"
+        className="w-full h-full object-cover absolute"
+      />
+      <div className="flex justify-between items-center p-5 absolute top-0 w-full">
+        <p className="text-3xl text-white">9:41</p>
+        <img
+          className="h-6 filter brightness-0 invert"
+          src="headerContainer.png"
+          alt=""
+        />
+      </div>
+      <div className="absolute top-28 w-full flex items-center justify-center flex-col">
+        <p className="text-7xl text-white font-bold">Audio</p>
+        <p className="text-3xl text-white py-10">
+          It's modular and designed to last
+        </p>
+      </div>
+      <div className="absolute w-full flex flex-col items-center bottom-0">
+        <AuthButton mode="login" />
+        <p className="my-5 text-white">
+          Don't have an account?{" "}
+          <span
+            onClick={handleSignUp}
+            className="text-green-600 cursor-pointer"
+          >
+            Sign Up Here
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignScreen;
--- src/components/SignUpScreen.tsx
+++ src/components/SignUpScreen.tsx
@@ -1,34 +1,48 @@
 import { useNavigate } from "react-router-dom";
 import AuthButton from "./AuthButton";
+
 const SignUpScreen = () => {
   const navigate = useNavigate();
 
   const handleBackToLoginPage = () => {
     navigate("/");
   };
-    return (
-        <div className="w-full h-screen flex items-center justify-center flex-col">
-          <img
-            src="/backgroundImageLoginScreen.svg"
-            alt="background image"
-            className="w-full h-full object-cover"
-          />
-          <div className="absolute top-28 w-full flex items-center justify-center flex-col">
-            <p className="text-9xl">Audio 2 </p>
-            <p className="text-3xl py-10">It's modular and designed to last</p>
-            <AuthButton mode="signup" />
-            <p className="mt-5">
-              Already have an account?{" "}
-              <span
-                onClick={handleBackToLoginPage}
-                className="text-green-600 cursor-pointer"
-              >
-                Sign In Here
-              </span>
-            </p>
-          </div>
-        </div>
-      );
-}
 
-export default SignUpScreen
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col relative">
+      <img
+        src="/backgroundImageLoginScreen.svg"
+        alt="background image"
+        className="w-full h-full object-cover absolute"
+      />
+      <div className="flex justify-between items-center p-5 absolute top-0 w-full">
+        <p className="text-3xl text-white">9:41</p>
+        <img
+          className="h-6 filter brightness-0 invert"
+          src="headerContainer.png"
+          alt=""
+        />
+      </div>
+      <div className="absolute top-28 w-full flex items-center justify-center flex-col">
+        <p className="text-7xl text-white font-bold">Audio</p>
+        <p className="text-3xl text-white py-10">
+          It's modular and designed to last
+        </p>
+      </div>
+      <div className="absolute w-full flex flex-col items-center bottom-0">
+        <AuthButton mode="signup" />
+        <p className="my-5 text-white">
+          Already have an account?{" "}
+          <span
+            onClick={handleBackToLoginPage}
+            className="text-green-600 cursor-pointer"
+          >
+            Sign In Here
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpScreen;
